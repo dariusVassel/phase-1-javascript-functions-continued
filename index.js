a13ab94f61334444a59678a359f0df9e4dd656a2
+ <+>
2021-07-07 10:47:07 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+//Function 
+function saturdayFun(someActivity = "roller-skate"){
+    return `This Saturday, I want to ${someActivity}!`
+}
+
+//Function expression
+const mondayWork = function(someActivity = "go to the office"){
+    return `This Monday, I will ${someActivity}.`
+}
+
+function wrapAdjective(someString = "*"){
+    return function(someValue = "special"){
+        return `You are ${someString}${someValue}${someString}!`;
+    }
+}
+
+console.log(wrapAdjective("lololo")("a dedicated programmer"));
